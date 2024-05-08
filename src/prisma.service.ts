fe3b6e5ab5e483808bfb52d99a04fa48b5456f93
pramesh <pramesh@hood.live>
2024-05-09 00:47:40 +0530

    Update Package.json

--- src/prisma.service.ts
+++ src/prisma.service.ts
@@ -1,9 +1,17 @@
-import { Injectable, OnModuleInit } from "@nestjs/common";
+import { Injectable, OnModuleInit, Logger } from "@nestjs/common";
 import { PrismaClient } from "@prisma/client";
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
+  private readonly logger = new Logger(PrismaService.name);
+
   async onModuleInit() {
-    await this.$connect();
+    try {
+      await this.$connect();
+      this.logger.log("Prisma client connected successfully");
+    } catch (error) {
+      this.logger.error("Error connecting to Prisma client", error.stack);
+      throw error; // Rethrow the error to halt application initialization
+    }
   }
 }
