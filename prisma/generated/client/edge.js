407eb6eafe40912b359151dcafea889e73d011f8
pramesh <pramesh@hood.live>
2024-06-02 15:38:32 +0530

    tsconfig changes updated

--- prisma/generated/client/edge.js
+++ prisma/generated/client/edge.js
@@ -171,7 +171,7 @@ const config = {
     "db"
   ],
   "activeProvider": "postgresql",
-  "postinstall": true,
+  "postinstall": false,
   "inlineDatasources": {
     "db": {
       "url": {
--- prisma/generated/client/index.js
+++ prisma/generated/client/index.js
@@ -172,7 +172,7 @@ const config = {
     "db"
   ],
   "activeProvider": "postgresql",
-  "postinstall": true,
+  "postinstall": false,
   "inlineDatasources": {
     "db": {
       "url": {
