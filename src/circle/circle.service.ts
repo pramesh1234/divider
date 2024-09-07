e772bdabc77f9c32658ed2252a1e45bb1d76883f
pramesh <pramesh@hood.live>
2024-09-07 15:26:21 +0530

    changes for location in response

--- src/circle/circle.service.ts
+++ src/circle/circle.service.ts
@@ -34,7 +34,7 @@ async getBroadcastByUserId(userId:string):Promise<any>{
     FROM "Broadcast" 
     INNER JOIN "Circle" ON "Circle"."broadcast_id" = "Broadcast"."broadcast_id" where receiver_id = ${userId}
 `;
-const coorOfUser = await this.prismaService.$queryRaw`SELECT location from User where user_id = userId`
+const coorOfUser = await this.prismaService.$queryRaw`SELECT location from  "User" Where "user_id" = ${userId}`
 const dis1 = 'SRID=4326;${b_location_string}'
 const dis =  await this.prismaService.$queryRaw`SELECT
     ST_Distance(
