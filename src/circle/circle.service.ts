cc18aa5d7cc39c63ec588edf13eadedf9aa01ba8
pramesh <pramesh@hood.live>
2024-09-07 16:56:18 +0530

    changes for location in response

--- src/circle/circle.service.ts
+++ src/circle/circle.service.ts
@@ -49,7 +49,7 @@ const coorOfUser = await this.prismaService.$queryRaw`SELECT ST_AsText(location)
             const dis = await this.prismaService.$queryRaw`
                 SELECT ST_Distance(
                     ST_GeogFromText(${`SRID=4326;POINT(${coordinates})`}),
-                    ST_GeogFromText(${`SRID=4326;${coorOfUser[0].location_string}`})
+                    ST_GeogFromText(${`SRID=4326;${coorOfUser[0].location_string.replace(","," ")}`})
                 ) AS distance_meters;
             `;
     
